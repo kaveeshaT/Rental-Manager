9ee241dcaba714c4c778b525ee625992e8a532c1
kaveeshaT <kaveeshatharushika@gmail.com>
2019-06-29 13:49:37 +0530

    commit 1

--- Product_Rental/Models/Movie.cs
+++ Product_Rental/Models/Movie.cs
@@ -8,6 +8,6 @@ namespace Product_Rental.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set;}
     }
 }
